3bf5ec687ac382d9b9a125333b41b9202b4045b4
abaibik <abaibik@gmail.com>
2021-09-25 16:50:14 +0200

    renders single item

---
+++ src/components/ExpenceList.vue
@@ -0,0 +1,32 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Date</th>
+        <th scope="col">Category</th>
+        <th scope="col">Value</th>
+      </tr>
+    </thead>
+    <tbody>
+      <ExpenceListRow
+        v-for="idx in items.length"
+        :key="idx"
+        :index="idx"
+        :item="items[idx - 1]"
+      />
+    </tbody>
+  </table>
+</template>
+
+<script>
+import ExpenceListRow from "./ExpenceListRow.vue";
+
+export default {
+  name: "ExpenceList",
+  components: { ExpenceListRow },
+  props: {
+    items: Array,
+  },
+};
+</script>
