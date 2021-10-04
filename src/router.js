95dd66c955f564b10eafa9cede689b40a231cca6
Aleksandra Baibik <abaibik@gmail.com>
2021-10-04 09:02:49 +0200

    remove extra dirs

---
+++ src/router.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "@/App.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "App",
+    component: App,
+  },
+
+  {
+    path: "/add/payment/:category?value=:value",
+    name: "Add",
+    component: App,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
