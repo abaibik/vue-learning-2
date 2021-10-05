876d6e6dbbdac25b7dc722ba3cb953095be654de
Aleksandra Baibik <abaibik@gmail.com>
2021-10-05 21:13:27 +0200

    links UI

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="container">
     <h1 class="mb-5"><ins>Expence list</ins></h1>
-    <button
-      type="button"
-      class="btn btn-outline-secondary mb-5"
-      @click="$store.commit('showDialog')"
-    >
-      Add new cost
-    </button>
-    <AddDialog />
-    <router-link
-      :to="{
-        name: 'Add',
-        params: { category: 'Transport' },
-        query: { value: 456 },
-      }"
-      >Add regular payment</router-link
-    >
+    <div class="button-and-links d-flex mb-5 align-items-baseline gap-3">
+      <button
+        type="button"
+        class="btn btn-outline-secondary"
+        @click="$store.commit('showDialog')"
+      >
+        Add new cost
+      </button>
 
+      <router-link
+        class="text-decoration-none"
+        :to="{
+          name: 'Add',
+          params: { category: 'Transport' },
+          query: { value: 456 },
+        }"
+        >Add regular payment</router-link
+      >
+    </div>
+
+    <AddDialog />
     <Pagination v-if="$store.getters.pageCount >= 2" />
     <ExpenceList :items="currentPageItems" />
     <Pagination v-if="pageCount >= 2" />
