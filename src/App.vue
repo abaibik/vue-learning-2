74de7ac54f42298830e82b8bd4f64f873f23ac50
Aleksandra Baibik <abaibik@gmail.com>
2021-10-18 17:57:01 +0200

    chart

--- src/App.vue
+++ src/App.vue
@@ -25,18 +25,22 @@
     <Pagination v-if="$store.getters.pageCount >= 2" />
     <ExpenceList :items="currentPageItems" />
     <Pagination v-if="pageCount >= 2" />
+    <div class="flex-chart d-flex w-100 justify-content-center">
+      <div class="chart mt-5 w-50 h-50"><Chart /></div>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 import AddDialog from "./components/AddDialog.vue";
+import Chart from "./components/Chart.vue";
 import ExpenceList from "./components/ExpenceList.vue";
 import Pagination from "./components/Pagination.vue";
 
 export default {
   name: "App",
-  components: { ExpenceList, AddDialog, Pagination },
+  components: { ExpenceList, AddDialog, Pagination, Chart },
   computed: mapGetters(["currentPageItems", "pageCount"]),
   mounted() {
     this.$store.dispatch("fetchData");
